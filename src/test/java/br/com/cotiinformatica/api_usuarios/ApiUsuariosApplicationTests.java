06dde9aef04e9d2457d75501e8e8bdd2538db25f
sergiocotiazure20265-ops <sergiocotiazure20265@gmail.com>
2026-07-27 19:00:51 -0300

    versão inicial

---
+++ src/test/java/br/com/cotiinformatica/api_usuarios/ApiUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica.api_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
